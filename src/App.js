f824c12c3b383fc3ad8238e7a33e796faefc7b72
Oroxhimaru <hm51562@gmail.com>
2023-11-11 13:44:16 +0500

    little work done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+     username:"hassan",
+     caption:"yeah yeah",
+     imageUrl:"https://i.pinimg.com/1200x/09/8d/40/098d40876eddf9bd843eb3863e125028.jpg"
+    },
+    {
+      username:"naruto",
+     caption:"rasengan",
+     imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSarWMNR_1akZESyvStTIerY47_bYEuevSNnw"
+    }
+  ]);
+
   return (
     <div className="app">
       <div className="app_header">
@@ -11,8 +25,14 @@ function App() {
           className="app_headerImage"
         />
       </div>
+       
+       {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+       }
 
-      <Post />
+
     </div>
   );
 }
